e8cd36ed1f494e3a2da9e7c740e5d905441ba9bb
bujanabatista@gmail.com <bujanabatista@gmail.com>
2024-06-10 20:02:27 +0700

    loginregister

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import db from "./config/Database.js";
+import Users from "./model/usermodel.js";
+import router from "./routes/index.js";
+dotenv.config();
+const app = express();
+
+try {
+  await db.authenticate();
+  console.log("konek");
+} catch (error) {
+  console.error(error);
+}
+
+app.use(express.json());
+app.use(router);
+
+app.listen(5000, () => console.log("server 5000"));
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { getUsers, Register, Login, updateUser } from "../controllers/Users.js";
+
+const router = express.Router();
+
+router.get("/users", getUsers);
+router.post("/register", Register);
+router.post("/login", Login);
+
+export default router;
